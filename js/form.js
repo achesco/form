2804d50b597eb7c7e8b8ba2a55e21005f370c0c1
chesco <achesco@gmail.com>
2012-04-05 21:15:01 +0300

    Переименовал файл для соответствия названию пакета.

---
+++ js/form.js
@@ -0,0 +1,8 @@
+/**
+    @namespace Пакет классов для работы с формами.
+    @requires jquery 1.5+ (http://jquery.com/download/)
+    @requires ZForms 3.0.4+ (http://zforms.ru/download/)
+    @requires base64.js
+    @requires $.identify
+ */
+var form = {};
