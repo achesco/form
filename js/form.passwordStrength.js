4e6da05e196dbd8ece49e40875c4f4f6c669421a
chesco <achesco@gmail.com>
2012-04-05 21:17:49 +0300

    Функция form.passwordStrength для рассчета силы пароля (простенькая).

---
+++ js/form.passwordStrength.js
@@ -0,0 +1,41 @@
+/**
+    Determine presumable password strength: value form 0 to 5
+    @static
+ */
+form.passwordStrength = function(val, minLength) {
+
+  this.countRegexp = function(val, rex) {
+    var match = val.match(rex);
+    return match ? match.length : 0;
+  }
+  
+  var len = val.length;
+
+  if (len < minLength) return 0;
+    
+  var nums = this.countRegexp(val, /\d/g),
+    lowers = this.countRegexp(val, /[a-z]/g),
+    uppers = this.countRegexp(val, /[A-Z]/g),
+    specials = len - nums - lowers - uppers;
+  
+  if (nums == len || specials == len)
+    return 1;
+  
+  var strength = 0;
+  if (nums) { strength+= 2; }
+  if (lowers) { strength+= uppers? 2 : 1; }
+  if (uppers) { strength+= lowers? 2 : 1; }
+  if (specials) { strength+= 2; }
+  if (len > 10) { strength+= 1; }
+  
+  if (strength > 0 && strength <= 2)
+    return 2;
+  else if (strength > 2 && strength <= 3)
+    return 3;
+  else if (strength > 3 && strength <= 4)
+    return 4;
+  else if (strength > 4)
+    return 5;
+  else
+    return 0;
+};
